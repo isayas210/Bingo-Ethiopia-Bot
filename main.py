92485e20a75da350479242a41a229c7179b48e4e
isayas210 <isayasemana098@gmail.com>
2026-04-16 04:40:59 +0300

    Refactor web server setup and bot polling

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 import os
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.utils import executor
 from aiohttp import web
 
-# TOKEN kee Environment Variable Render irraa fudhata
 TOKEN = os.getenv("TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# Mini App akka banamuuf (index.html tajaajila)
 async def handle(request):
     try:
         return web.FileResponse('index.html')
     except:
         return web.Response(text="Bingo Ethiopia Live is Running")
 
-async def start_web_server():
+async def on_startup(app):
+    # Webhook ykn Polling kallaattiin akka hin banne
+    print("Bot is starting...")
+    asyncio.create_task(dp.start_polling())
+
+async def create_app():
     app = web.Application()
     app.router.add_get('/', handle)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    # Render "PORT" nuuf kenna, yoo dhabame 10000 fayyadama
-    port = int(os.environ.get("PORT", 10000))
-    site = web.TCPSite(runner, '0.0.0.0', port)
-    await site.start()
+    app.on_startup.append(on_startup)
+    return app
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    # Web server bota wajjiin akka dammaqu godha
-    loop.create_task(start_web_server())
-    # Bota kee kaasi
-    executor.start_polling(dp, skip_updates=True)
+    port = int(os.environ.get("PORT", 10000))
+    web.run_app(create_app(), host='0.0.0.0', port=port)
