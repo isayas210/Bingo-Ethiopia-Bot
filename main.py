c2477a9b9aa9848664b4757817331ce5f80099df
isayas210 <isayasemana098@gmail.com>
2026-04-16 10:24:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher, executor
 
-# Token kee isa sirrii
+# Token kee
 API_TOKEN = '8692359063:AAHteqfebC808tTmj6qvIdjiVJIXoXRTf4c'
 
 logging.basicConfig(level=logging.INFO)
@@ -10,10 +10,12 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 async def on_startup(_):
-    # Webhook moofaa qulqulleessuun Conflict ni balleessa
-    await bot.delete_webhook()
+    # Webhook hunda ni qulqulleessa, update-oota duraan conflict uuman ni dhabamsiisa
+    await bot.delete_webhook(drop_pending_updates=True)
+    print("---------------------------------")
     print("Bot-ni Bingo hojii jalqabeera!")
+    print("---------------------------------")
 
 if __name__ == '__main__':
-    # skip_updates=True ergaawwan conflict uuman bira darba
+    # skip_updates=True Conflict balleessuuf ni gargaara
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
