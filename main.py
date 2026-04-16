84d1d6f4eeab53743e0e8d7417cae2e0d679a342
isayas210 <isayasemana098@gmail.com>
2026-04-16 10:06:32 +0300

    Simplify on_startup and clean up main.py
    Refactor bot startup function and remove unused code.

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import WebAppInfo
+from aiogram import Bot, Dispatcher, executor
 
-# Token kee isa ati naaf ergite
 API_TOKEN = '8692359063:AAHteqfebC808tTmj6qvIdjiVJIXoXRTf4c'
-
-# Logging qindaysuuf
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# Yeroo bot-ni jalqabu webhook qulqulleessuuf
-async def on_startup(dp):
+async def on_startup(_):
+    # Webhook moofaa qulqulleessuun Conflict ni balleessa
     await bot.delete_webhook()
-    print("---------------------------------")
     print("Bot-ni Bingo hojii jalqabeera!")
-    print("---------------------------------")
-
-# Ergaa Command /start
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    # HUBADHU: Linkii HTML kee isa bareedaa Render irratti 'Static Site' uumte as galchi
-    # Yoo ammaaf linkii hin qabdu ta'e kanumaan yaali
-    web_app_url = "https://bingo-ethio.netlify.app" # Fakkeenyaaf
-    
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton(
-        text="🎮 Bingo Taphadhu", 
-        web_app=WebAppInfo(url=web_app_url)
-    ))
-    
-    await message.answer(
-        f"Baga nagaaan dhufte {message.from_user.first_name}!\n\n"
-        "Bingo taphachuuf button 'Bingo Taphadhu' jedhu tuqi.",
-        reply_markup=markup
-    )
 
 if __name__ == '__main__':
+    # skip_updates=True ergaawwan moofaa conflict uuman ni darba
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
